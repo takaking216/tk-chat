3f88f34ec029ab19e39153393d6716750ca79372
takaking216 <58413762+takaking216@users.noreply.github.com>
2019-12-24 16:25:53 +0900

    message

--- Gemfile
+++ Gemfile
@@ -39,6 +39,7 @@ group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platform: :mri
   gem 'did_you_mean'
+  gem 'pry-rails'
 end
 
 group :development do
@@ -56,3 +57,5 @@ gem 'haml-rails'
 gem 'font-awesome-sass'
 gem 'devise'
 gem 'rails-i18n', '~> 5.1'
+gem 'carrierwave'
+gem 'mini_magick'
--- app/models/group.rb
+++ app/models/group.rb
@@ -1,5 +1,23 @@
 class Group < ApplicationRecord
   has_many :group_users
   has_many :users, through: :group_users
+  has_many :messages
+
   validates :name, presence: true, uniqueness: true
+
+  def show_last_message
+    if (last_message = messages.last).present?
+      last_message.content? ? last_message.content : '画像が投稿されています'
+    else
+      'まだメッセージはありません。'
+    end
+  end
+  
+  def show_last_message_created_at
+    if (last_message_created_at = messages.last.created_at).present?
+      last_message_created_at.strftime("%Y年%m月%d日 %H時%M分")
+    else
+      ''
+    end
+  end
 end
