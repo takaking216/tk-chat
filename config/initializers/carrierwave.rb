33f9c36983d1332fa920d9743b52dc382a09d803
takaking216 <takaking216@yahoo.co.jp>
2020-02-06 20:52:46 +0900

    Update carrierwave.rb

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -13,5 +13,5 @@ CarrierWave.configure do |config|
   }
 
   config.fog_directory  = 'tk-chat2020'
-  config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/ここにバケット名を入れます'
+  config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/tk-chat2020'
 end
